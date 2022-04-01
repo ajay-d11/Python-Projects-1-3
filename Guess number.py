79fde2d62aad28fdbcd8f72655c88e5b19b83fb4
ajay-d11 <102684372+ajay-d11@users.noreply.github.com>
2022-04-01 09:28:20 -0700

    Add files via upload

---
+++ Guess number.py
@@ -0,0 +1,35 @@
+import random
+
+which_game = input("Pcik a game, user_game or computer_game: ")
+if "user_game":
+    def guess(x):
+        rand_num = random.randint(1,x)
+        guess = 0
+        while guess != rand_num:
+            guess = int(input(f"Guess a number betweem 1 and {x}"))
+            print(guess)
+            if guess < rand_num:
+                print("sorry, guess again. Too low.")
+            elif guess > rand_num:
+                print("Sorry, guess again. Too high.")
+
+        print(f"You have guessed the number {rand_num} correctly")
+
+else:
+        def comp_guess(x):
+            low = 1
+            high = x
+            feedback = ""
+            while feedback != "c": 
+                if low != high:
+                    guess = random.randint(low, high)
+                else:
+                    guess = low       
+                    feedback = input(f"Is {guess} too high (H), too low (L), or correct (C) ?? "). lower()
+                if feedback == "H":
+                    high = guess - 1
+                elif feedback == "L":
+                    low = guess + 1
+            print(f"The computer guessed your number, {guess} correctly")
+
+guess(10)
